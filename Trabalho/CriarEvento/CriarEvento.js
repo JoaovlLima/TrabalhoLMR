cd587d92057af914d6d5c342d4077762b713744b
JoaovlLima <124844047+JoaovlLima@users.noreply.github.com>
2023-06-19 11:21:37 -0300

    Add files via upload

--- Trabalho/CriarEvento/CriarEvento.js
+++ Trabalho/CriarEvento/CriarEvento.js
@@ -6,3 +6,38 @@ function toggleMenu() {
     var divLogin = document.getElementById("loginEfetuado")
     divLogin.classList.toggle("active");
   }
+  function gerarCard(event) {
+    event.preventDefault();
+  
+    var titulo = document.getElementById("titulo").value;
+    var descricao = document.getElementById("descricao").value;
+    var imagem = document.getElementById("imagem").files[0];
+  
+    var card = document.createElement("div");
+    card.classList.add("card");
+  
+    var tituloElement = document.createElement("h2");
+    tituloElement.textContent = titulo;
+  
+    var descricaoElement = document.createElement("p");
+    descricaoElement.textContent = descricao;
+  
+    if (imagem) {
+      var imagemElement = document.createElement("img");
+      imagemElement.src = URL.createObjectURL(imagem);
+      card.appendChild(imagemElement);
+    }
+  
+    card.appendChild(tituloElement);
+    card.appendChild(descricaoElement);
+  
+    var divCards = document.getElementById("meus-cards");
+    divCards.appendChild(card);
+  
+    document.getElementById("titulo").value = "";
+    document.getElementById("descricao").value = "";
+    document.getElementById("imagem").value = null;
+  }
+  
+  var formulario = document.getElementById("meu-formulario");
+  formulario.addEventListener("submit", gerarCard);
